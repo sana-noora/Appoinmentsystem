9a34c2daf9a5cd6e1efa01ea648679764573865c
sana113AZ <sana.321abuzahra@gmail.com>
2026-03-04 22:08:48 +0200

    Add PostgreSQL connection class

---
+++ src/main/java/connectDB/connectDB.java
@@ -0,0 +1,20 @@
+package connectDB;
+import java.sql.*;
+public class connectDB {
+	 public static void main(String[] args) {
+
+	        String url = "jdbc:postgresql://localhost:1234/Appoinment"; 
+	        String user = "postgres"; 
+	        String password = "123456";
+
+	        try {
+	            Connection conn = DriverManager.getConnection(url, user, password);
+	            System.out.println("Connected to PostgreSQL database!");
+	        } catch (SQLException e) {
+	            System.out.println("Connection failed!");
+	            e.printStackTrace();
+	        }
+	    }
+	
+
+}
