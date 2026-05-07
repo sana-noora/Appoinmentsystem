17673833158565ccef64bf46cf28990e13d9c57e
sana113AZ <sana.321abuzahra@gmail.com>
2026-05-07 19:16:17 +0300

    solve issues

--- doc/tag-search-index.js
+++ doc/tag-search-index.js
@@ -1,1 +1,1 @@
-tagSearchIndex = [{"l":"Constant Field Values","h":"","u":"constant-values.html"}];updateSearchResults();
+const tagSearchIndex = [{"l":"Constant Field Values","h":"","u":"constant-values.html"}];updateSearchResults();
--- src/main/java/connectDB/connectDB.java
+++ src/main/java/connectDB/connectDB.java
@@ -1,15 +1,21 @@
 package connectDB;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class connectDB {
-    private static final String URL  = "jdbc:postgresql://localhost:5432/appointment_system";
-    private static final String USER = "postgres";
-    private static final String PASS = "123456";
+
+    private static final String URL  =
+            "jdbc:postgresql://localhost:5432/appointment_system";
+
+    private static final String USER =
+            System.getenv("DB_USER");
+
+    private static final String PASS =
+            System.getenv("DB_PASS");
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASS);
     }
 }
+
